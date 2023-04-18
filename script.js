eaa3a1feb2e44b3303403deadd7b9876e7bf4d91
Mausomi <129541838+Mausomi@users.noreply.github.com>
2023-04-19 00:20:09 +0530

    JS_ROCK_PPR_SCISSOR

--- script.js
+++ script.js
@@ -1,47 +1,75 @@
-let timerDisplay = document.querySelector('.timerDisplay');
-let stopBtn = document.getElementById('stopBtn');
-let startBtn = document.getElementById('startBtn');
-let resetBtn = document.getElementById('resetBtn');
+//storing in variables
+let userScore= 0;
+let compScore= 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p =document.querySelector(".result > p");
+const rock_div =document.getElementById("r");
+const paper_div =document.getElementById("p");
+const scissor_div =document.getElementById("s");
 
-let msec = 0o0;
-let secs = 0o0;
-let mins = 0o0;
-
-let timerId = null;
-
-startBtn.addEventListener('click', function(){
-    if(timerId !== null){
-        clearInterval(timerId);
-    }
-    timerId = setInterval(startTimer, 10);
-});
-
-stopBtn.addEventListener('click', function(){
-    clearInterval(timerId);
-});
-
-resetBtn.addEventListener('click', function(){
-    clearInterval(timerId);
-    timerDisplay.innerHTML = `00 : 00 : 00`;
-    msec = secs = mins = 0o0;
-});
-
-function startTimer(){
-    msec++;
-    if(msec == 100){
-        msec = 0;
-        secs++;
-        if(secs == 60){
-            secs = 0;
-mins++;
-        }
-    }
-
-    let msecString = msec < 10 ? `0${msec}` : msec;
-    let secsString = secs < 10 ? `0${secs}` : secs;
-    let minsString = mins < 10 ? `0${mins}` : mins;
-    
-
-    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
-
+//COMPUTER CHOICES
+function getcomputerChoice(){
+    const choices= ['r', 'p', 's'];
+    const randomNumber = Math.floor (Math.random() * 3);
+    return choices[randomNumber];
 }
+function convertToWord(letter){
+    if (letter== "r") return "Rock";
+    if (letter== "p") return "Paper";
+}
+
+
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = userChoice + "beats" + convertToWord(computerChoice) + "You win";
+}
+function lose(){
+    console.log("LOST");
+}
+function draw(){
+    console.log("DRAW");
+}
+
+
+//USER CHOICES
+function game(userChoice){
+    const computerChoice= getcomputerChoice();
+    switch(userChoice + computerChoice) {
+        case "rp":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+            case "rp":
+            case "ps":
+            case "sr" :
+                lose(userChoice, computerChoice);
+                break;
+                case "rr":
+                case "pp":
+                case "ss":
+                   draw(userChoice, computerChoice);
+                    break;
+
+        
+
+    }
+}
+function main(){
+    rock_div.addEventListener('click', function(){
+        game("r");
+    })
+    paper_div.addEventListener('click', function(){
+        game("p");
+    })
+    scissor_div.addEventListener('click', function() {
+        game("s");
+        
+    })
+}
+main();
