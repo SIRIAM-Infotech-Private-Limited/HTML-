60c950b7b6bfb87da268b3dcc4a1cd292db9e8b8
Mausomi <129541838+Mausomi@users.noreply.github.com>
2023-04-21 18:12:46 +0530

    EVENT_FUNCTION

--- REACT_JS/App.js
+++ REACT_JS/App.js
@@ -1,32 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {Component} from 'react'
 
-function App() {
-  //Component inside a component
-  function Apple(){
-    return(<div>Hello </div>)
+// import React, {Component} from 'react'
+// import User from './User'
+
+// function App() {
+//   //Component inside a component
+//   function Apple(){
+//     return(<div>Hello </div>)
+//   }
+//   return (
+//     <div className="App">
+//       <h1>Hello Universe</h1>
+//       <User></User>
+//       {Apple()} 
+//       <Apple></Apple>
+//       <Components></Components>
+//     </div>
+//   );
+// }
+// class Components extends Component
+// {
+//     render()
+//     {
+//         return(
+//             <h1>Hello from user</h1>
+//         )
+//     }
+
+// }
+
+function App(){
+  function Apple()
+  {
+   alert("Function called");
   }
-  return (
-    <div className="App">
-      <h1>Hello Universe</h1>
-      <User></User>
-      {Apple()} 
-      <Apple></Apple>
-      <Components></Components>
+  return(
+    <div className='App'>
+      <h1>Hello World !</h1>
+      <button onclick={() => alert("hello")}>Click Me</button>
     </div>
   );
 }
-class Components extends Component
-{
-    render()
-    {
-        return(
-            <h1>Hello from user</h1>
-        )
-    }
-
-}
-
-
 export default App;
