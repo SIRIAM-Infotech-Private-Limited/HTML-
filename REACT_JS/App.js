58edf112fb8dba0fda147c24b9b6cdaa703e0738
Mausomi <129541838+Mausomi@users.noreply.github.com>
2023-04-23 19:12:34 +0530

    STATE WITH FUNCTIONAL COMPONENT

--- REACT_JS/App.js
+++ REACT_JS/App.js
@@ -1,45 +1,25 @@
+//State with functional component
+
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-// import React, {Component} from 'react'
-// import User from './User'
 
-// function App() {
-//   //Component inside a component
-//   function Apple(){
-//     return(<div>Hello </div>)
-//   }
-//   return (
-//     <div className="App">
-//       <h1>Hello Universe</h1>
-//       <User></User>
-//       {Apple()} 
-//       <Apple></Apple>
-//       <Components></Components>
-//     </div>
-//   );
-// }
-// class Components extends Component
-// {
-//     render()
-//     {
-//         return(
-//             <h1>Hello from user</h1>
-//         )
-//     }
 
-// }
-
-function App(){
-  function Apple()
-  {
-   alert("Function called");
+function App() {
+  const [data, setData] = useState(0)
+  function updateData() {
+    setData(data+1)
   }
-  return(
-    <div className='App'>
-      <h1>Hello World !</h1>
-      <button onclick={() => alert("hello")}>Click Me</button>
+
+  return (
+    <div className="App">
+      <h1>{data}</h1>
+      
+      <button onClick={updateData} >Update Data</button>
     </div>
   );
 }
+
+
 export default App;
