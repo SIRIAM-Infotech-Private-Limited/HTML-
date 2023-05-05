093f92d2a4a8245161e3a312c8ce778d39cf7833
Mausomi <129541838+Mausomi@users.noreply.github.com>
2023-05-05 17:46:32 +0530

    NETFLIX_CLONE

---
+++ NETFLIx/HomeScreen.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./HomeScreen.css";
+import Nav from './Nav';
+import Banner from './Banner';
+import requests from './Request';
+import Row from './Row';
+
+function HomeScreen() {
+  return (
+    <div className ='homeScreen'>
+        <Nav />
+        <Banner></Banner>
+
+         <Row 
+         title="NETFLIX ORIGINALS"
+         fetchUrl= {requests.fetchNetflixOriginals}
+         isLargeRow
+         /> 
+         
+         <Row title= "Top Rated" fetchUrl={requests.fetchTopRated} />
+         
+         <Row title= "Action Movies" fetchUrl={requests.fetchActionMovies} />
+         <Row title= "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+         <Row title= "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+         <Row title= "Rommance Movies" fetchUrl={requests.fetchRomanceMovies} />
+         <Row title= "Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+
+    </div>
+
+  )
+}
+
+export default HomeScreen;
