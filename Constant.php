4b6d7e8e0101bf9cb555995e9304031e1efe1fe4
Mausomi <129541838+Mausomi@users.noreply.github.com>
2023-05-11 22:44:54 +0530

    PHP

---
+++ Constant.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// case-sensitive constant name
+define("WELCOME", "Welcome to World!");
+echo WELCOME  ;
+define("Fruits", [
+    "Banana",
+    "apple",
+    "Mango"
+  ] );
+  echo Fruits[0];
+?> 
+
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+
---
+++ Datatypes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = "Hello world!";
+$y = 'Hello world!';
+$a = 1000;
+$fruits = array("Banana", "Apple", "Mango");
+
+echo $x;
+echo "<br>"; 
+echo $y;
+echo "<br>";
+var_dump($a);
+echo "<br>";
+var_dump($fruits);
+?>
+
+</body>
+</html>
